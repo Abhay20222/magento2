6d8f2b00007c7b410eb5930301f07b042fd17f9e
Abhay20222 <112698889+Abhay20222@users.noreply.github.com>
2023-01-09 13:44:26 +0530

    Create start.sh

---
+++ scripts/start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /var/www/html/
+sudo chmod -R 777 magento2/
+sudo chown -R www-data:www-data magento2/
+cd magento2/
+php bin/magento setup:di:compile
+php bin/magento setup:install --base-url=http://magento.rtd.squareops.co.in/ --db-host=magento-rds.rtd.squareops.co.in --db-name=magentodb --db-user=magento2 --db-password=password --admin-firstname=Magento --admin-lastname=User --admin-email=admin@mystore.com --admin-user=admin --admin-password=admin123 --language=en_US --currency=USD --timezone=America/Chicago --use-rewrites=1 --backend-frontname="admin" --search-engine=elasticsearch7 --elasticsearch-host=magento-es.rtd.squareops.co.in --elasticsearch-port=9200
+sudo chmod -R 777 magento2/var
